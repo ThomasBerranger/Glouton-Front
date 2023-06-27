548a0f0a6a6996cde91cff77643ba26c050db18b
Thomas Berranger <tberranger@hotmail.fr>
2023-06-27 12:26:30 +0200

    tailwind added, footer in progress

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [],
+  theme: {
+    fontFamily: {
+      title: ['Moirai One', 'cursive'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
