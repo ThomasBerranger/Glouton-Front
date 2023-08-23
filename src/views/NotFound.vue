592f21b0bfbbf296a29b187eca02ef12e403e865
Thomas Berranger <tberranger@hotmail.fr>
2023-08-23 12:05:37 +0200

    Not found page designed

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,15 @@
 <script setup>
+import {RouterLink} from "vue-router";
 </script>
 
 <template>
-  <h2>Not foun page</h2>
+  <div class="w-screen h-full flex flex-1 flex-col justify-center">
+
+    <div class="w-full">
+      <img class="mx-auto h-40 w-auto" src="/public/logo.webp" alt="Your Company"/>
+      <h2 class="mt-5 text-center leading-9 tracking-tight text-gray-900">Cette page n'existe pas.</h2>
+    </div>
+
+  </div>
+
 </template>
